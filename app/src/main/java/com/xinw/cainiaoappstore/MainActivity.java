5fc0b23617df6e917369ded806ab5628960bc210
D9ingMac <D9ingMac@D9ing.com>
2017-08-08 19:46:16 +0800

    time:2017.8.7 area:company commit log:加入butterknife 完成DrawerLayout 集成了NavigationView

--- app/src/main/java/com/xinw/cainiaoappstore/MainActivity.java
+++ app/src/main/java/com/xinw/cainiaoappstore/MainActivity.java
@@ -1,13 +1,80 @@
 package com.xinw.cainiaoappstore;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+
+    @BindView(R.id.app_name)
+    TextView appName;
+    @BindView(R.id.dr_layout)
+    DrawerLayout drLayout;
+    @BindView(R.id.navigaview_left)
+    NavigationView navigaviewLeft;
+
+    private View headView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        drLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+        headView = navigaviewLeft.getHeaderView(0);
+        headView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        navigaviewLeft.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_app_update:
+                        Toast.makeText(MainActivity.this, "更新", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_message:
+                        Toast.makeText(MainActivity.this, "消息", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_setting:
+                        Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
 }
